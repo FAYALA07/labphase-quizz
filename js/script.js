e10c2a151cc409be46eea5c3b0ad43c89784140a
FAYALA07 <mohamedfayala05@gmail.com>
2024-07-25 11:45:47 +0100

    gomycode

---
+++ js/script.js
@@ -0,0 +1,132 @@
+const questions = [
+  {
+    question: "what's the capital of tunisia?",
+    answers: [
+      { text: "Tunis", correct: true },
+      { text: "kairouan", correct: false },
+      { text: "Gabes", correct: false },
+      { text: "Sousse", correct: false },
+    ],
+  },
+  {
+    question: "which is the smallest continent in the world?",
+    answers: [
+      { text: "Australia", correct: true },
+      { text: "Europe", correct: false },
+      { text: "Asia", correct: false },
+      { text: "Africa", correct: false },
+    ],
+  },
+  {
+    question: "who won the last euro?",
+    answers: [
+      { text: "Italy", correct: false },
+      { text: "Spain", correct: true },
+      { text: "Germany", correct: false },
+      { text: "England", correct: false },
+    ],
+  },
+  {
+    question: "where is zitouna moasque",
+    answers: [
+      { text: "Tunis", correct: true },
+      { text: "Jandouba", correct: false },
+      { text: "Tozer", correct: false },
+      { text: "non above", correct: false },
+    ],
+  },
+  {
+    question: "what is the most populated country in the world",
+    answers: [
+      { text: "India", correct: true },
+      { text: "Usa", correct: false },
+      { text: "China", correct: false },
+      { text: "Russia", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+var currentQuestionIndex = 0;
+var score = 0;
+
+function startQuizz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  var currentQuestion = questions[currentQuestionIndex];
+  var questioNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questioNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML =
+    "you scoored " + score + " out of " + questions.length;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", function () {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuizz();
+  }
+});
+
+startQuizz();
+var popup = confirm("lets test your geography knowledge");
